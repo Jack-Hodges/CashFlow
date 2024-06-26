bb54b70d2076f3a03b42dd80b66965614c01e8c4
Jack Hodges <jackhodg3s@gmail.com>
2024-06-26 15:39:23 +1000

    Initial Commit

---
+++ CashFlow/CashFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  CashFlowApp.swift
+//  CashFlow
+//
+//  Created by Jack Hodges on 26/6/2024.
+//
+
+import SwiftUI
+
+@main
+struct CashFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CashFlow/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CashFlow
+//
+//  Created by Jack Hodges on 26/6/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
